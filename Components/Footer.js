9bb8e5316ae27766658b6b966df5001e765af4e0
Mizaan Shaikh <mizaannuvoraa@gmail.com>
2025-06-18 15:49:13 +0530

    Footer Updated 2

--- Components/Footer.js
+++ Components/Footer.js
@@ -10,21 +10,21 @@ const Footer = () => {
     <div className={`md:pb-4 ${isThankYouPage ? 'pb-3' : 'pb-14'} py-4 px-4 text-[13px] lato bg-[#F2F2F2] text-black flex flex-row items-center justify-center space-x-7 text-center`}>
       <Link
         href="https://properties.emaar.com/en/privacy-policy/"
-        className="mid_text hover:underline tracking-widest font-semibold"
+        className="mid_text text-gray-600 hover:underline tracking-widest font-semibold"
       >
         PRIVACY POLICY
       </Link>
       <span className="border-l h-4 border-black"></span>
       <Link
         href="https://properties.emaar.com/en/terms-and-conditions/"
-        className="mid_text hover:underline tracking-widest uppercase font-semibold"
+        className="mid_text text-gray-600 hover:underline tracking-widest uppercase font-semibold"
       >
         TERMS & CONDITIONS
       </Link>
        <span className="border-l h-4 border-black"></span>
-     <p className=" mid_text tracking-widest uppercase font-semibold">Designed by  <Link
+     <p className=" mid_text text-gray-600 tracking-widest uppercase font-semibold">Designed & Developed by <Link
         href="https://www.nuvoraa.com/"
-        className="mid_text hover:underline tracking-widest uppercase font-black"
+        className="mid_text hover:underline text-gray-800 tracking-widest uppercase font-black"
       >Nuvoraa</Link></p>
         
     </div>
